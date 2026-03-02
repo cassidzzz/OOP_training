e9543697e683b915540c7588d242bb4339dcd3d4
Денис Доменяко <denisdomenyako@mail.ru>
2026-03-02 14:31:59 +0300

    Добавьте файлы проекта.

---
+++ OOP_training/Program.cs
@@ -0,0 +1,12 @@
+﻿using Ch1.Tasks;
+
+
+//-----------------------------------------------
+//Task 1
+
+Book book1 = new Book("qwe", "qwee", 5, 50);
+Book book2 = new Book("asd", "asdf", 10, 100);
+
+book1.DisplayInfo();
+book2.DisplayInfo();
+
---
+++ OOP_training/Tasks/Task1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ch1.Tasks
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        private double Price;
+        private int pages;
+        public double price 
+        { 
+            get { return Price; }
+            set { Price = value; }
+        }
+        public int Pages
+        {
+            get { return pages; }
+            set { pages = value > 0 ? value : 1; }
+        }
+
+    public Book(string title, string author, int pages, double price_)
+        {
+            Title = title;
+            Author = author;
+            Pages = pages;
+            Price = price_;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Название: " + Title);
+            Console.WriteLine("Автор: " + Author);
+            Console.WriteLine("Страниц: " + Pages);
+            Console.WriteLine("Цена: " + Price);
+        }
+    }
+}
